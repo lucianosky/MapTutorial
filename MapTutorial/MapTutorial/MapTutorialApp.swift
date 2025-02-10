7c94ea6ed8860ac805118632f52246e988a5764f
Luciano Sclovsky <luciano.sky@gmail.com>
2025-02-10 18:46:24 -0300

    Created Xcode project

---
+++ MapTutorial/MapTutorial/MapTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTutorialApp.swift
+//  MapTutorial
+//
+//  Created by Luciano Sclovsky on 10/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MapTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
